600cd22b0ec14719c57c1be5f8e157efb0bb3a47
Brian Young <brianyoung@stanford.edu>
2012-01-18 09:53:10 -0800

    Convert Stanford Wilbur to sub-theme of Stanford Framework

--- block.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?><?php print $block->extraclass; ?>">
-	<?php if (!empty($block->subject)): ?>
-		<h3><?php print $block->subject ?></h3>
-	<?php endif;?>
-	<div class="content">
-		<?php print $block->content ?>
-	</div>
-</div>
--- search-theme-form.tpl.php
+++
@@ -1,7 +0,0 @@
-<div class="search-item">
-	<input class="search-input form-text" type="text" maxlength="128" name="search_theme_form" id="edit-search_theme_form" value="" title="Enter the terms you wish to search for" />
-</div>
-<div class="search-item">
-	<input class="search-submit" type="submit" name="op" value=""  />
-</div>
-<?php print $search['hidden']; ?>
